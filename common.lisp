7116ea57dc6ed070e87c5d801e9b3651352ab650
Pierre Thierry <pierre@nothos.net>
2011-07-13 00:07:35 +0200

    Added basic lambda functions for boolean algrebra.

--- common.lisp
+++ common.lisp
@@ -8,3 +8,15 @@
 (defvar *id* (make-expression '(lambda x x)))
 (defvar *delta* (make-expression '(lambda x (x x))))
 (defvar *omega* (make-expression (list *delta* *delta*)))
+
+
+#| Booleans |#
+
+(defvar *true* (make-expression '(lambda x (lambda y x))))
+(defvar *false* (make-expression '(lambda x (lambda y y))))
+
+(defvar *if* (make-expression '(lambda cond (lambda then (lambda else (cond then else))))))
+
+(defvar *and* (make-expression '(lambda p (lambda q (p q p)))))
+(defvar *or* (make-expression '(lambda p (lambda q (p p q)))))
+(defvar *not* (make-expression `(lambda x (x ,*false* ,*true*))))
