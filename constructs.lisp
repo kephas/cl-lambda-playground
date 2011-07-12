eba2becbec6e1ce193c1988a845632072c8fbf83
Pierre Thierry <pierre@nothos.net>
2011-07-12 07:30:17 +0200

    Implemented lambda expressions and commodity expression-building function.

---
+++ constructs.lisp
@@ -0,0 +1,34 @@
+(defclass expression () ())
+
+(defclass variable (expression)
+  ((name :accessor var-name :initarg :name)))
+
+(defclass abstraction (expression)
+  ((variable :accessor abs-var :initarg :var)
+   (body :accessor abs-body :initarg :body)))
+
+(defclass application (expression)
+  ((function :accessor app-fun :initarg :fun)
+   (argument :accessor app-arg :initarg :arg)))
+
+
+(defun make-expression (sexpr)
+  (typecase sexpr
+    ((or character string)
+     (make-instance 'variable :name sexpr))
+    (symbol
+     (make-instance 'variable :name (string-downcase (symbol-name sexpr))))
+    (cons
+     (case (first sexpr)
+       ((lambda) (make-instance 'abstraction :var (make-expression (second sexpr)) :body (make-expression (third sexpr))))
+       (t (make-applications-chain
+	   (make-instance 'application :fun (make-expression (first sexpr)) :arg (make-expression (second sexpr)))
+	   (cddr sexpr)))))))
+
+(defun make-applications-chain (fun sexpr)
+  (if (null sexpr)
+      fun
+      (make-applications-chain (make-instance 'application
+					      :fun fun
+					      :arg (make-expression (first sexpr)))
+			       (rest sexpr))))
