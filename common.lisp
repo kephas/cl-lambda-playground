0f6b36c4ff753b6f92d104b918fd41cfb0eb8697
Pierre Thierry <pierre@nothos.net>
2011-07-25 20:25:25 +0200

    Corrected typo in Church sub.

--- common.lisp
+++ common.lisp
@@ -67,7 +67,7 @@
 (defvar *c_plus* (make-expression '(lambda m (lambda n (m f (n f x))))))
 (defvar *c_succ* (make-expression '(lambda n (f (n f x)))))
 (defvar *c_pred* (make-expression '(lambda n (lambda f (lambda x (n (lambda g (lambda h (h (g f)))) (lambda u x) (lambda u u)))))))
-(defvar *c_sub* (make-expression '(lambda m (lambda n (n pred m))) (bind-value "pred" *pred* nil)))
+(defvar *c_sub* (make-expression '(lambda m (lambda n (n pred m))) (bind-value "pred" *c_pred* nil)))
 (defvar *c_mult* (make-expression '(lambda m (lambda n (lambda f (lambda x (m (n f) x)))))))
 (defvar *c_exp* (make-expression `(lambda m (lambda n (n (* m) ,(church-num 1)))) (bind-value "*" *c_mult* nil)))
 
